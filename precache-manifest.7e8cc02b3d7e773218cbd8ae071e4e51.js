806bbead3f9db0c60ee3bba5180922313ffc37b3
Irina <irina.potrebko7@gmail.com>
2020-05-21 13:52:17 +0300

    Updates

---
+++ precache-manifest.7e8cc02b3d7e773218cbd8ae071e4e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da13f6608d7d646e2bcb6d63bdd1150b",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "765b0e6206e72f26c3ad",
+    "url": "/Counter/static/css/main.b5910e25.chunk.css"
+  },
+  {
+    "revision": "34eae134a0ec3a4088de",
+    "url": "/Counter/static/js/2.9414c5a5.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/Counter/static/js/2.9414c5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765b0e6206e72f26c3ad",
+    "url": "/Counter/static/js/main.e7e3ff96.chunk.js"
+  },
+  {
+    "revision": "9db62ce4ba4feb94cd59",
+    "url": "/Counter/static/js/runtime-main.ae9bbd7a.js"
+  }
+]);
---
+++ src/SetDisplay/SetDisplay.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './SetDisplay.css';
+import Button from "../ButtonArea/Button/Button";
+
+class SetDisplay extends React.Component {
+
+    onInputStart = (e) => {
+        this.props.onStartValueChange(e.currentTarget.value)
+    };
+    onInputMax = (e) => {
+        this.props.onMaxValueChange(e.currentTarget.value)
+    };
+
+
+    render = () => {
+
+
+        let inputClass = this.props.disabledSet ? 'redinput' : 'input';
+
+        return (<div className="box">
+                <div className="display">
+                    <div>
+                        <span>start value</span>
+                        <input value={this.props.startCount}
+                               onChange={this.onInputStart}
+                               className={inputClass}
+                               type='number'/>
+                    </div>
+                    <div>
+                        <span>max value</span> <input value={this.props.maxValue}
+                                                      onChange={this.onInputMax}
+                                                      className={inputClass}
+                                                      type='number'/>
+                    </div>
+                </div>
+                <div>
+                    <Button title={'set'}
+                            onClickHandler={this.props.onValueChange}
+                            disabledButton={this.props.disabledSet}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default SetDisplay;
